c6c4d8148e9e35ffbd0ec3d5ce2e0395cb3f4914
seph <joseph.baron@devpartners.co>
2020-02-29 00:15:49 +0800

    added test

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -39,6 +39,7 @@ namespace UnitTest
         }
 
         [TestMethod]
+
         public void TestsIfReturnCashAsExpected()
         {
             VendingMachine vm = new VendingMachine();
@@ -57,7 +58,41 @@ namespace UnitTest
             var products = vm.DisplayAllItems();
 
             Assert.AreEqual(typeof(List<Product>), products.GetType());
-
         }
+
+        [DataTestMethod]
+        [DataRow("1", "Coke", 25)]
+        [DataRow("2", "Pepsi", 35)]
+        [DataRow("3", "Soda", 45)]
+        [DataRow("4", "Bottled water", 15)]
+        [DataRow("5", "chocolate bar", 20.25)]
+        [DataRow("6", "Chewing gum", 10.25)]
+        public void When_Selecting_Products(string itemNumber, string productName, double amount)
+        {
+            VendingMachine vm = new VendingMachine();
+
+            var result = vm.ItemExists(itemNumber);
+
+            Assert.AreEqual(true, result);
+        }
+
+        [DataTestMethod]
+        [DataRow("1", "Coke", "25", "$25.00")]
+        [DataRow("2", "Pepsi", "35", "$35.00")]
+        [DataRow("3", "Soda", "45", "$45.00")]
+        [DataRow("4", "Bottled water", "15", "$15.00")]
+        [DataRow("5", "chocolate bar", "20.25", "$20.25")]
+        [DataRow("6", "Chewing gum", "10.25", "$10.25")]
+        public void When_CancelingItem_SelectProductToCancel_TO_Have_Refund(string itemNumber, string productName, string amount,string expectedAmount)
+        {
+            VendingMachine vm = new VendingMachine();
+
+            var result = vm.Money.AddMoney(amount);
+
+            var results = vm.Money.MoneyInMachine.ToString("C");
+
+            Assert.AreEqual(expectedAmount, results);
+        }
+
     }
 }
